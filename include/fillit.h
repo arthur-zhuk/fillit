f4074b552e03d6c49cdfc602d0f994b9a3c9b26f
Zachary Smith <zsmith@e1z2r2p6.42.us.org>
2016-10-08 21:00:28 -0700

    added symbol_check test to input file

--- include/fillit.h
+++ include/fillit.h
@@ -6,7 +6,7 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/07 21:57:52 by zsmith            #+#    #+#             */
-/*   Updated: 2016/10/08 18:12:43 by zsmith           ###   ########.fr       */
+/*   Updated: 2016/10/08 20:57:51 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int		check_grid_lines(char *input);
 int		last_char_nl(char *input);
+int		symbol_check(char *x);
 
 char	*line_check(char *av);
 
