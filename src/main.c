de3425adbf09e791e00e0f887459af54667ec1c3
Zachary Smith <zsmith@e1z2r2p10.42.us.org>
2016-10-10 14:55:46 -0700

    all tests done.  Working on character encoding.

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "../include/libft.h"
+#include "../include/fillit.h"
+
+int		main(int argc, char **argv)
+{
+	char	*ret;
+
+	if (argc != 2)
+	{
+		ft_putstr("usage: fillit source_file");
+		return (0);
+	}
+	ret = input_data(argv[1]);
+	if (!ret)
+		return (0);
+	prnt_str2grid(ret);
+	// printf("%d double \\n's \n", line_check(argv[1]));
+	// if (line_cnt(argv[1]))
+	// printf("%d - This tetri is 16 chars in size\n", line_cnt(argv[1]));
+	// else
+		// printf("This tetri isn't 16 chars in size");
+
+	return (0);
+}
