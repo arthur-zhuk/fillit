c5735f5128ed26694b297679da3e9306a72ff03a
Arthur Zhuk <arthurzhuk@gmail.com>
2016-10-11 19:37:36 -0700

    test work

--- src/main.c
+++ src/main.c
@@ -3,25 +3,25 @@
 
 int		main(int argc, char **argv)
 {
-	/*char	*ret;*/
+	char	*ret;
 	argc = 0; // edit this later
-	argv = NULL; // edit this later
+	//argv = NULL; // edit this later
 
 	char	*z;
 
-	z = (char *)malloc(33);
-	ft_strcpy(z, "......##.##.....");
-	controller(z);
+	//z = (char *)malloc(33);
+	//ft_strcpy(z, "......##.##.....");
+	//controller(z);
 
 	/*if (argc != 2)*/
 	/*{*/
 		/*ft_putstr("usage: ./fillit source_file");*/
 		/*return (0);*/
 	/*}*/
-	/*ret = input_data(argv[1]);*/
-	/*if (!ret)*/
-		/*return (0);*/
-	/*prnt_str2grid(ret);*/
+	ret = input_data(argv[1]);
+	if (!ret)
+		return (0);
+	//prnt_str2grid(ret);
 	return (0);
 }
 
